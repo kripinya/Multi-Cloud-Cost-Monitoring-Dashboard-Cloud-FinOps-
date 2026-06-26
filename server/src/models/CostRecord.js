26273bd24047a15c28a18d5a6e03d7ba6e4e0660
ananyakripinya <ananyakripinya@gmail.com>
2026-06-26 09:57:03 +0530

    creating billing data schema

---
+++ server/src/models/CostRecord.js
@@ -0,0 +1,50 @@
+//a mongoose schema defining the shape of every billing record of our database
+//like a table definition in sql
+const mongoose = require('mongoose');
+
+const costRecordSchema = new mongoose.Schema({
+    provider: {
+        type: String,
+        required: true,
+        enum: ['AWS', 'Azure', 'GCP']
+    },
+    service: {
+        type: String,
+        required: true
+    },
+    region: {
+        type: String,
+        required: true
+    },
+    cost: {
+        type: Number,
+        required: true
+    },
+    currency: {
+        type: String,
+        default: USD
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    usageQuantity: {
+        type: Number,
+        default: 0
+    },
+    tags: {
+        project: { type: String, default: 'default' },
+        team: { type: String, default: 'engineering' },
+        environment: {
+            type: String,
+            enum: ['production', 'staging', 'development'],
+            default: 'production'
+        }
+    }
+}, {
+    timestamps: true //creats createdAt and updatedAt field automatically
+});
+costRecordSchema.index({ provider: 1, date: -1 }); //index on provider and date for faster access
+costRecordsSchema.index({ date: -1 });//
+//export the model"costRecord" becomes collection name "costRecords"in mongoDB
+module.exports = mongoose.model('CostRecord', costRecordSchema);
